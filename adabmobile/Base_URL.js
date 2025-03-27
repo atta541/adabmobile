2947052d9b232b017c48113e56e8d917fc7dd1c1
atta541 <attareh542@gmail.com>
2025-03-28 04:51:01 +0500

    remove invoices folder

--- adabmobile/Base_URL.js
+++ adabmobile/Base_URL.js
@@ -1,7 +1,6 @@
-// const Base_URL = 'http://10.0.2.2:5000';
-// export default Base_URL;
+const Base_URL = 'http://10.0.2.2:5000';
 
-const Base_URL = 'http://192.168.1.8:5000';
+// const Base_URL = 'http://192.168.1.8:5000';
 
 // const Base_URL = 'http://adabbackend-a01u0rvum-atta-ur-rehmans-projects-9ba26c3f.vercel.app/';
 export default Base_URL;
